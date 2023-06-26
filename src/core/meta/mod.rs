9747db57af82321327213878e4c7778b203b632a
IS2511 <is2511@bk.ru>
2023-06-26 23:34:52 +0300

    feat(meta): fill in app meta

--- src/core/meta/mod.rs
+++ src/core/meta/mod.rs
@@ -15,3 +15,22 @@ pub struct AppMetaAdditionalResources {
     pub faq: Option<&'static str>,
     pub help: Option<&'static str>,
 }
+
+
+impl Default for AppMeta {
+    fn default() -> Self {
+        AppMeta {
+            name: "ToolboxIS",
+            version: semver::Version::parse(env!("CARGO_PKG_VERSION"))
+                .expect("CARGO_PKG_VERSION is not a valid SemVer, which should be impossible"),
+            authors: vec!["IS2511 <is2511@is2511.com>"],
+            homepage: "https://is2511.com/",
+            additional: AppMetaAdditionalResources {
+                docs: None,
+                download: None,
+                faq: None,
+                help: None,
+            },
+        }
+    }
+}
